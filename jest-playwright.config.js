45044fab5921dbd6fb08a7923d65693a98b428ea
Andris Borbás <andris.borbas@gmail.com>
2021-05-04 20:17:41 +0200

    Change playwright to headless

--- jest-playwright.config.js
+++ jest-playwright.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  serverOptions: {
-    command: "next start -p 3000",
-    port: 3000,
-  },
-  browsers: ["chromium"],
-  launchOptions: {
-    headless: false,
-  },
+	serverOptions: {
+		command: "next start -p 3000",
+		port: 3000,
+	},
+	browsers: ["chromium"],
+	launchOptions: {
+		headless: true,
+	},
 };
