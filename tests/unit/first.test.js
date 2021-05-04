12d98f68d7043615adfea6e98bd5dfd21765dc9d
Cuslika <mondokbence@gmail.com>
2021-05-04 20:51:03 +0200

    Pushing Handler unit tests

--- tests/unit/first.test.js
+++
@@ -1,9 +0,0 @@
-import handler from "lib/api/handler";
-
-describe("Handler", () => {
-  it("should throw error, if bad argument given", () => {
-    expect(() => {
-      handler({ GTE: () => {} });
-    }).toThrow("Invalid method in mapping");
-  });
-});
